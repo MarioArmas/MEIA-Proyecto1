fa70ec5eecb0dcb30f81fb54928406ffbb6ed417
Mario Armas <mario.r.armas@gmail.com>
2022-09-24 17:10:44 -0600

    Add get file function

--- Proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -5,6 +5,12 @@
 
 package com.mycompany.proyecto1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author mario
@@ -12,6 +18,35 @@ package com.mycompany.proyecto1;
 public class Proyecto1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    }
+    
+    public static ArrayList<String> getFile(String path) {
+        ArrayList<String> data = new ArrayList<String>();
+        File file = new File(path);
+        
+        if (file.exists()) {
+            try {
+                FileReader fileReader = new FileReader(file);
+                BufferedReader fileToRead = new BufferedReader(fileReader);
+                String line = fileToRead.readLine();
+
+                while (line != null) {
+                    if (!"".equals(line)) {
+                        data.add(line);
+                    }
+                    
+                    line = fileToRead.readLine();
+                }
+
+                fileReader.close();
+                fileToRead.close();
+            } catch (IOException ex) {
+                String strError = ex.getMessage();
+                return data;
+            }
+        }
+
+        return data;
     }
 }
